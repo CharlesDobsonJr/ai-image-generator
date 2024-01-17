2c7b60aeae9159b60dae13778a7e11adfa751996
Charles Dobson <charlesmdobsonjr@gmail.com>
2024-01-17 01:46:24 -0500

    Added default photo and text.

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
+
+const ImageGenerator = () => {
+    return (
+        <div className="ai-image-generator">
+            <div className="header">Ai image <span>generator</span></div>
+        </div>
+    )
+}
+
+export default ImageGenerator
